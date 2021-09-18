55ec83cd8e988726e5bca67ae06a9b500349b5af
SheikhRashed <devrashed06@gmail.com>
2021-09-19 00:02:22 +0600

    seted data to localstorage

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // UI Variables
 
 const container = document.querySelector('.container');
-const seats = document.querySelector('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.querySelector('#count');
 const total = document.querySelector('#total');
 const movieSelect = document.querySelector('#movie');
@@ -11,9 +11,21 @@ let ticketPrice = +movieSelect.value;
 
 const selectedSeats = document.querySelectorAll('.row .seat.selected').length;
 
+// Save selected movie data and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update Total Seats & Total Seat booking price
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+  // localStorage.setItem('selectedSeats', seatsIndex);
+
   count.innerText = selectedSeats.length;
   total.innerText = selectedSeats.length * ticketPrice;
 }
@@ -21,6 +33,7 @@ function updateSelectedCount() {
 // Movie Select
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
